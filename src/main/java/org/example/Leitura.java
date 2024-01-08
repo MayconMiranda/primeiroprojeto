9c9c55c9f64d262254e0d73b52dc950e4b545362
MayconMiranda <maycon12.miranda@gmail.com>
2024-01-08 20:56:03 -0300

    leitura com o Scanner

---
+++ src/main/java/org/example/Leitura.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Leitura {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("digite seu filme favorito: ");
+        String filme = leitura.nextLine();
+
+        System.out.println("qual ano de lançamento: ");
+        int anoDeLancamento = leitura.nextInt();
+        System.out.println("diga sua avaliação pro filme: ");
+        double avaliacao = leitura.nextDouble();
+
+        System.out.println(filme);
+        System.out.println(anoDeLancamento);
+        System.out.println(avaliacao);
+    }
+}
