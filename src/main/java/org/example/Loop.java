ba865bc0c656ed036cbfb81768cc6d6b44561ad2
MayconMiranda <maycon12.miranda@gmail.com>
2024-01-08 21:23:35 -0300

    criando a classe loop

---
+++ src/main/java/org/example/Loop.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Loop {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        double mediaAvaliacao = 0;
+        double nota = 0;
+
+        for (int i = 0; i < 3; i++){
+            System.out.println("diga sua avaliação pro filme: ");
+            nota = leitura.nextDouble();
+            mediaAvaliacao += nota;
+        }
+
+        System.out.println("media de avaliações: "+ mediaAvaliacao/3);
+
+
+    }
+}
